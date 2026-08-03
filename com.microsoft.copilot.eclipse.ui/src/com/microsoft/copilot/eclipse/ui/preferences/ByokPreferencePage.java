cd4a6cc1dc4e89c71ba7f9d730ca26e853aa93ba
Dietrich Travkin <10887297+travkin79@users.noreply.github.com>
2026-08-03 04:27:12 +0200

    Fix #364 - Use image registries (#365)

--- com.microsoft.copilot.eclipse.ui/src/com/microsoft/copilot/eclipse/ui/preferences/ByokPreferencePage.java
+++ com.microsoft.copilot.eclipse.ui/src/com/microsoft/copilot/eclipse/ui/preferences/ByokPreferencePage.java
@@ -49,10 +49,10 @@ import com.microsoft.copilot.eclipse.core.CopilotCore;
 import com.microsoft.copilot.eclipse.core.FeatureFlags;
 import com.microsoft.copilot.eclipse.core.lsp.protocol.byok.ByokModel;
 import com.microsoft.copilot.eclipse.core.lsp.protocol.byok.ByokModelProvider;
+import com.microsoft.copilot.eclipse.ui.CopilotImages;
 import com.microsoft.copilot.eclipse.ui.CopilotUi;
 import com.microsoft.copilot.eclipse.ui.chat.services.ByokService;
 import com.microsoft.copilot.eclipse.ui.utils.SwtUtils;
-import com.microsoft.copilot.eclipse.ui.utils.UiUtils;
 
 /**
  * BYOK (Bring Your Own Key) preference page for configuring AI model providers. This page allows users to add, remove,
@@ -177,7 +177,7 @@ public class ByokPreferencePage extends PreferencePage implements IWorkbenchPref
     disabledCompositeLayout.marginWidth = 0;
     disabledCompositeLayout.marginHeight = 0;
     disabledComposite.setLayout(disabledCompositeLayout);
-    WrappableIconLink.createWithCustomizedImage(disabledComposite, "/icons/information.png",
+    WrappableIconLink.create(disabledComposite, CopilotImages.getImage(CopilotImages.IMG_INFORMATION),
         Messages.preferences_page_byok_disabled_tip);
     contentComposite = createByokView(pageStateStack);
     updatePageState();
@@ -269,15 +269,8 @@ public class ByokPreferencePage extends PreferencePage implements IWorkbenchPref
     viewerStack.setLayout(stackLayout);
     GridData stackData = new GridData(SWT.FILL, SWT.FILL, true, true);
     viewerStack.setLayoutData(stackData);
-    enabledIcon = UiUtils.buildImageFromPngPath("/icons/chat/keep.png");
-    disabledIcon = PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_ETOOL_DELETE);
-
-    viewerStack.addDisposeListener(e -> {
-      if (enabledIcon != null && !enabledIcon.isDisposed()) {
-        enabledIcon.dispose();
-        enabledIcon = null;
-      }
-    });
+    enabledIcon = CopilotImages.getImage(CopilotImages.IMG_CHAT_KEEP);
+    disabledIcon = CopilotImages.getSharedImage(ISharedImages.IMG_ETOOL_DELETE);
 
     // Create tree viewer composite
     treeComposite = createTreeViewer(viewerStack);
